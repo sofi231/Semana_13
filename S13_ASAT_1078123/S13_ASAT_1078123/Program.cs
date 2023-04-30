1d57a2cbb34848ac69e6e0da9758432ea81c5e50
Sofi <sofia12.to@gmailcom>
2023-04-30 07:54:10 -0600

    Primer ejercicio, clase persona

--- S13_ASAT_1078123/S13_ASAT_1078123/Program.cs
+++ S13_ASAT_1078123/S13_ASAT_1078123/Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Globalization;
+
+class Persona
+{
+    private string nombre;
+    private string apellido;
+    private int edad;
+    private double cm;
+
+    public Persona(string nombre, string apellido, int edad, double cm)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.cm = cm;
+    }
+
+    public string NombreCompleto()
+    {
+        return nombre + " " + apellido;
+    }
+
+    public double mts()
+    {
+        return cm / 100;
+    }
+
+    public void MostrarDatos()
+    {
+        Console.WriteLine("Nombre completo: " + NombreCompleto());
+        Console.WriteLine("Edad: " + edad);
+        Console.WriteLine("Altura en metros: " + mts());
+    }
+
+    static void Main(string[] args)
+    {
+        Persona persona = new Persona("Sofía", "Asturias", 18, 161.5);
+        persona.MostrarDatos();
+    }
+}
